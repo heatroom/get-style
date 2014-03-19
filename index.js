fad1a7d9368fbc04bf014c94c73786a06567c2a0
heatroom <2257505815@qq.com>
2014-03-19 22:24:55 +0800

    add getStyleSize.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var defView = window.document.defaultView;
 
-var getStyle = defView && defView.getComputedStyle ?
+exports.getStyle = defView && defView.getComputedStyle ?
   function( elem ) {
     return defView.getComputedStyle( elem, null );
   } :
@@ -8,4 +8,10 @@ var getStyle = defView && defView.getComputedStyle ?
     return elem.currentStyle;
   };
 
-module.exports = getStyle;
+// get a number from a string, not a percentage
+exports.getStyleSize = function( value ) {
+  var num = parseFloat( value );
+  // not a percent like '100%', and a number
+  var isValid = value.indexOf('%') === -1 && !isNaN( num );
+  return isValid && num;
+}

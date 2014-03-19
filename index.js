27e2a6565837d7efc2a0a31d48d67656d8a8ad7b
heatroom <2257505815@qq.com>
2014-03-19 21:41:59 +0800

    first commits.

---
+++ index.js
@@ -0,0 +1,11 @@
+var defView = window.document.defaultView;
+
+var getStyle = defView && defView.getComputedStyle ?
+  function( elem ) {
+    return defView.getComputedStyle( elem, null );
+  } :
+  function( elem ) {
+    return elem.currentStyle;
+  };
+
+module.exports = getStyle;
